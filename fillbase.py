889fea1fd06ebcef7f08ebdb201566c3f422816f
I Bo <ilya112358@gmail.com>
2022-08-15 21:44:32 +0300

    Fillbase: change workflow to loop in main

--- fillbase.py
+++ fillbase.py
@@ -3,42 +3,34 @@ import pyinputplus as pyip
 import header
 
 def fill_wordbase():
-    done = False
-    while not done:
-        inf = input('\nInfinitive to add? ')
-        if inf in infs:
-            print('Already exists!')
-            continue
-        pres = input('Presens? ')
-        past = input('Preteritum? ')
-        supin = input('Supinum? ')
-        trans = input('Translation? ')
-        verb = [inf, pres, past, supin, trans]
-        print(verb)
-        correct = input('Correct? [Y/Other] ')
-        if correct == 'Y' or correct == 'y':
-            verbs.append(verb)
-            verbs.sort(key=lambda verb: verb[0])
-            infs.append(inf)
-            f.seek(0) # rewrite
-            json.dump(verbs, f)
+    inf = input('\nInfinitive to add? ')
+    if inf in infs:
+        print('Already exists!')
+        return
+    pres = input('Presens? ')
+    past = input('Preteritum? ')
+    supin = input('Supinum? ')
+    trans = input('Translation? ')
+    verb = [inf, pres, past, supin, trans]
+    print(verb)
+    if pyip.inputYesNo('Correct? ') == 'yes':
+        verbs.append(verb)
+        verbs.sort(key=lambda verb: verb[0])
+        infs.append(inf)
+        f.seek(0) # rewrite
+        json.dump(verbs, f)
 
 def del_el():
-    done = False
-    while not done:
-        inf = input('\nInfinitive to delete? ')
-        try:
-            x = infs.index(inf)
-        except ValueError:
-            print('Not in the list!')
-            continue
-        print(verbs[x])
-        correct = input('Correct? [Y/Other] ')
-        if correct == 'Y' or correct == 'y':
-            verbs.pop(x)
-            infs.pop(x)
-        inp = input('\nAnother? [Y/Other] ')
-        done = (inp != 'Y' and inp != 'y')
+    inf = input('\nInfinitive to delete? ')
+    try:
+        x = infs.index(inf)
+    except ValueError:
+        print('Not in the list!')
+        return
+    print(verbs[x])
+    if pyip.inputYesNo('Delete this entry? ') == 'yes':
+        verbs.pop(x)
+        infs.pop(x)
 
 def makemock():
     mock = []
@@ -62,23 +54,25 @@ if __name__ == "__main__":
     wordbase_mock = config['Path']['Mock']
     with open(wordbase, encoding='utf-8') as f:
         verbs = json.load(f)
-    print(f'\n{len(verbs)} verbs loaded\n')
     with open(wordbase_bak,'w', encoding='utf-8') as f:
         json.dump(verbs, f)
-    verbs.sort(key=lambda verb: verb[0])
-    infs = header.infinitives(verbs)
-    inp = pyip.inputNum('Choose 1 to del, 2 to input new, '
-                        '3 to make mock base ', min=1, max=3)
-    if inp == 1:
-        with open(wordbase,'w', encoding='utf-8') as f:
-            del_el()
-            json.dump(verbs, f)
-    elif inp == 2:
-        with open(wordbase,'w', encoding='utf-8') as f:
-            json.dump(verbs, f)
-            fill_wordbase()
-    elif inp == 3:
-        mock = makemock()
-        with open(wordbase_mock,'w', encoding='utf-8') as f:
-            json.dump(mock, f)
-    input('Press Enter to exit')
+    while True:
+        print(f'\n{len(verbs)} verbs loaded\n')
+        verbs.sort(key=lambda verb: verb[0])
+        infs = header.infinitives(verbs)
+        inp = pyip.inputNum('\nChoose 1 to del, 2 to input new, '
+                            '3 to make mock base, 4 to exit ', min=1, max=4)
+        if inp == 1:
+            with open(wordbase,'w', encoding='utf-8') as f:
+                del_el()
+                json.dump(verbs, f)
+        elif inp == 2:
+            with open(wordbase,'w', encoding='utf-8') as f:
+                json.dump(verbs, f)
+                fill_wordbase()
+        elif inp == 3:
+            mock = makemock()
+            with open(wordbase_mock,'w', encoding='utf-8') as f:
+                json.dump(mock, f)
+        elif inp == 4:
+            break
