cc05cf4f507e65b3ff0c886c700d8e9ddcc33f84
I Bo <ilya112358@gmail.com>
2022-10-06 22:42:02 +0300

    Build into single directory

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 call env\scripts\activate.bat
 pyinstaller --noconfirm practice.py
 pyinstaller --noconfirm fillbase.py
-xcopy /e /y dist C:\Users\Public\Downloads\dist
+xcopy /e /y dist\fillbase C:\Users\Public\Downloads\SvenskaJa
+xcopy /e /y dist\practice C:\Users\Public\Downloads\SvenskaJa
 pause
