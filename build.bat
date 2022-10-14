3a816147a62bc698d9ed66d3e05e5a1ea02cb703
I Bo <ilya112358@gmail.com>
2022-10-14 21:29:11 +0300

    Build windows exe into local subdirectory and gitignore it

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 call env\scripts\activate.bat
 pyinstaller --noconfirm practice.py
 pyinstaller --noconfirm fillbase.py
-xcopy /e /y dist\fillbase C:\Users\Public\Downloads\SvenskaJa
-xcopy /e /y dist\practice C:\Users\Public\Downloads\SvenskaJa
+xcopy /e /y dist\fillbase executable
+xcopy /e /y dist\practice executable
 pause
