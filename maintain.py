00991a251702c2e52ce798eada46ff6e55031ba3
I Bo <ilya112358@gmail.com>
2022-11-02 20:57:05 +0300

    Add English translation. Remove add_el.

--- maintain.py
+++ maintain.py
@@ -7,9 +7,9 @@ import sys
 import pyinputplus as pyip
 
 def infinitives():
-    """Pretty print infs"""
+    """Fancy print infs"""
     if not infs:
-        return []
+        return
     list_verbs = '\nList of verbs in the word base:\n'
     width = len(max(infs, key=len)) + 3 # 3 spaces wider than the longest
     line = '\n'
@@ -23,17 +23,17 @@ def infinitives():
     print(list_verbs)
 
 def lookup():
-    """Ask for an infinitive, look it up, print, return with found boolean."""
+    """Ask for an infinitive, print, return with found boolean"""
     inf = pyip.inputStr('\nInfinitive? ').casefold()
     if inf in infs:
         entry = True
         print(inf, verbs[inf])
     else:
         entry = False
-        print(f'[{inf}] is not in the word base')
+        print(f'"{inf}" is not in the word base')
     return inf, entry
 
-def del_el():
+def delete():
     """Delete a verb from the list"""
     inf, entry = lookup()
     if not entry:
@@ -45,34 +45,7 @@ def del_el():
     query = "DELETE FROM VerbTranslations WHERE Verb = ?"
     cur.execute(query, (inf,))
     conn.commit()
-    print(f'[{inf}] deleted from word base')
-
-def add_el():
-    """Add a verb to the list"""
-    inf, entry = lookup()
-    if entry:
-        if pyip.inputYesNo('Do you want to replace this entry? ') == 'no':
-            return
-    e1 = [inf, '', '', '']
-    if pyip.inputYesNo('Do you want to enter verb forms? ') == 'yes':
-        e1[1] = pyip.inputStr('Present? ').casefold()
-        e1[2] = pyip.inputStr('Past? ').casefold()
-        e1[3] = pyip.inputStr('Supine? ').casefold()
-        query = "INSERT OR REPLACE INTO VerbForms VALUES (?, ?, ?, ?)"
-        cur.execute(query, tuple(e1))
-    e2 = [inf, '']
-    if pyip.inputYesNo('Do you want to enter a translation? ') == 'yes':
-        e2[1] = pyip.inputStr('Russian translation? ')
-        query = "INSERT OR REPLACE INTO VerbTranslations VALUES (?, ?)"
-        cur.execute(query, tuple(e2))
-    if not e1[1] and not e2[1]:
-        return
-    print(e1 + e2[1:])
-    if pyip.inputYesNo('Add this entry? ') == 'no':
-        conn.rollback()
-    else:
-        conn.commit()
-        print(f'[{inf}] added to word base')
+    print(f'"{inf}" deleted from the word base')
 
 def export_csv():
     """Export word base to csv file"""
@@ -103,7 +76,7 @@ def import_csv():
     n_added, n_changed = 0, 0
     in_forms, in_trans = [], []
     for line in lines:
-        if len(line) != 5 or not line[0] or not any(line[1:]):
+        if len(line) != 6 or not line[0] or not any(line[1:]):
             print(f'Incorrect format in {line}')
             continue
         verb = []
@@ -115,11 +88,12 @@ def import_csv():
             print(f'Incorrect or empty verb form in {line}')
             continue
         verb.append(line[4].strip())
+        verb.append(line[5].strip())
         def ins_rep():
             if all(verb[1:4]):
                 in_forms.append(tuple(verb[:4]))
-            if verb[4]:
-                in_trans.append((verb[0],verb[4]))
+            if any(verb[4:6]):
+                in_trans.append((verb[0], verb[4], verb[5]))
             
         if verb[0] not in infs:
             ins_rep()
@@ -132,7 +106,7 @@ def import_csv():
                 n_changed += 1
     query = "INSERT OR REPLACE INTO VerbForms VALUES (?, ?, ?, ?)"
     cur.executemany(query, in_forms)
-    query = "INSERT OR REPLACE INTO VerbTranslations VALUES (?, ?)"
+    query = "INSERT OR REPLACE INTO VerbTranslations VALUES (?, ?, ?)"
     cur.executemany(query, in_trans)
     conn.commit()
     print(f'{n_added} verbs added, {n_changed} verbs changed')
@@ -167,18 +141,19 @@ def loadbase():
         END;
         CREATE TABLE IF NOT EXISTS VerbTranslations (
             Verb TEXT NOT NULL PRIMARY KEY,
-            Russian TEXT NOT NULL
+            Russian TEXT NOT NULL,
+            English TEXT NOT NULL
         );
         """
     cur.executescript(query)
     conn.commit()
     # emulate FULL OUTER JOIN
     query = """
-        SELECT Infinitive, Present, Past, Supine, Russian
+        SELECT Infinitive, Present, Past, Supine, Russian, English
         FROM VerbForms LEFT JOIN VerbTranslations
         ON VerbTranslations.Verb = VerbForms.Infinitive
         UNION ALL
-        SELECT Verb, Present, Past, Supine, Russian
+        SELECT Verb, Present, Past, Supine, Russian, English
         FROM VerbTranslations LEFT JOIN VerbForms
         ON VerbForms.Infinitive = VerbTranslations.Verb
         WHERE Present IS NULL
@@ -193,7 +168,7 @@ if __name__ == "__main__":
     conn = sqlite3.connect('wordbase.db')
     conn.execute("PRAGMA foreign_keys = ON")
     cur = conn.cursor()
-    tasks = (infinitives, lookup, del_el, add_el, export_csv, import_csv, end)
+    tasks = (import_csv, export_csv, infinitives, lookup, delete, end)
     while True:
         verbs = loadbase()
         infs = list(verbs)
@@ -202,13 +177,12 @@ if __name__ == "__main__":
                   'Add a verb or import from a text file!\n')
         print(f'\n{len(verbs)} verbs loaded from the word base\n')
         inp = pyip.inputNum('Choose a number to:'
-                            '\n[1] list all,'
-                            '\n[2] look up,'
-                            '\n[3] delete,'
-                            '\n[4] add new,'
-                            '\n[5] export to text file,'
-                            '\n[6] import from text file,'
-                            '\n[7] exit\n',
-                            min=1, max=7)
+                            '\n[1] import from wordbase.txt,'
+                            '\n[2] export to wordbase.txt,'
+                            '\n[3] list all,'
+                            '\n[4] look up,'
+                            '\n[5] delete,'
+                            '\n[6] exit\n',
+                            min=1, max=6)
         tasks[inp-1]()
         input('\nPress Enter to continue')
